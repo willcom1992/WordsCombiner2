8247e011a066ee615a82ec72c446c5cb45e105ec
willcom <takuma1992jpn@gmail.com>
2023-03-06 23:59:19 +0900

    :wrench: 設定ファイルの修正

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using WordsCombiner.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    );
 
 var app = builder.Build();
 

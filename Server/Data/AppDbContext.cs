3a5e928e1c5955f383d4a7b353adb0688c8c82bf
willcom <takuma1992jpn@gmail.com>
2023-03-06 23:57:33 +0900

    :sparkles: コンテキストクラスの作成

---
+++ Server/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WordsCombiner.Shared.Model;
+
+namespace WordsCombiner.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Word> Words { get; set; }
+
+    }
+}
